84a4d676821aad72a31e53bd2663e0c7fcd1724c
Liam Cariou <cariou.liam@orange.fr>
2022-11-03 19:54:40 +0100

    ajout nav et base html

---
+++ modif.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <title>Modifier un étudiant</title>
+</head>
+<body>
+<nav class="navbar" role="navigation">
+        <div class="nav-logo">
+            <a href="./"><img class="logo" src="./img/logo-nws.png" alt="Logo NWS"></a>
+        </div>           
+        <ul class="navbar-links">
+       <li class="navbar-link lien "><a href="ajout.php">Ajouter un étudiant</a></li>
+            <li class="navbar-link lien"><a href="liste.php">Liste des étudiants</a></li>
+            <li class="navbar-link lien"><a href="rechercher.php">Rechercher un étudiant</a></li>
+            <li class="navbar-link lien"><a href="filtrer.php">Filtrer les catégories</a></li>
+            <li class="navbar-link lien"><a href="logout.php">Déconnexion</a></li>
+        </ul>
+
+        <button class="burger">
+            <span class="bar"></span>
+        </button>
+    </nav>
+</body>
+</html>
