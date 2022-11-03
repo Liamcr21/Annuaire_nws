28ba45b788cd494ee7328774f11f8dbed9b02608
Liam Cariou <cariou.liam@orange.fr>
2022-11-03 20:16:13 +0100

    crud ajout modif liste supp

--- class/crud.php
+++ class/crud.php
@@ -50,3 +50,115 @@
                 return false;
             }
         }
+
+
+        public function voir_tout()
+        {
+            global $db;
+            $query = "select * from etudiant";
+            $result = mysqli_query($db->connection,$query);
+            return $result;
+        }
+
+        public function requette_id($id)
+        {
+            global $db;
+            $sql = "select * from etudiant where ID='$id'";
+            $data = mysqli_query($db->connection,$sql);
+            return $data;
+
+        }
+
+        public function modif()
+        {
+            global $db;
+
+            if(isset($_POST['bouton-modif']))
+            {
+                $ID = $_POST['ID'];
+                $nom = $db->check($_POST['nom']);
+                $prenom = $db->check($_POST['prenom']);
+                $tel = $db->check($_POST['tel']);
+                $mail = $db->check($_POST['mail']);
+                $age = $db->check($_POST['age']);
+                $adresse = $db->check($_POST['adresse']);
+                $ville = $db->check($_POST['ville']);
+                $codepostal = $db->check($_POST['codepostal']);
+                $annee = $db->check($_POST['annee']);
+                $spe = $db->check($_POST['spe']);
+                $situation = $db->check($_POST['situation']);
+                $commentaire = $db->check($_POST['commentaire']);
+
+                if($this->requette_modif($ID,$nom,$prenom,$tel,$mail,$age,$adresse,$ville,$codepostal,$annee,$spe,$situation,$commentaire ))
+                {
+                    $this->message_ok('<div class="bon"> Etudiant modifié avec succès </div>');
+                    header("location:liste.php");
+                }
+                else
+                {   
+                    $this->message_ok('<div class="erreur"> Erreur modification étudiant (contacter un admin) </div>');
+                }
+
+               
+            }
+        }
+
+        public function requette_modif($id,$nom,$prenom,$tel,$mail,$age,$adresse,$ville,$codepostal,$annee,$spe,$situation,$commentaire)
+        {
+            global $db;
+            $sql = "update etudiant set nom='$nom', prenom='$prenom', tel='$tel', mail='$mail', age='$age', adresse='$adresse', ville='$ville', codepostal='$codepostal', annee='$annee', spe='$spe', situation='$situation', commentaire='$commentaire' where ID='$id'";
+            $result = mysqli_query($db->connection,$sql);
+            if($result)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+
+        public function supp_et($id)
+        {
+            global $db;
+            $query = "delete from etudiant where ID='$id'";
+            $result = mysqli_query($db->connection,$query);
+            if($result)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public function message_ok($msg)
+        {
+            if(!empty($msg))
+            {
+                $_SESSION['Message']=$msg;
+            }
+            else
+            {
+                $msg = "";
+            }
+        }
+
+        public function affichage_message_ok()
+        {
+            if(isset($_SESSION['Message']))
+            {
+                echo $_SESSION['Message'];
+                unset($_SESSION['Message']);
+            }
+        }
+
+    }
+
+
+
+
+?>
