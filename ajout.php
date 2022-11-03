cd06ba7b9cbc447b0297866ccc49b1e4df813b62
Liam Cariou <cariou.liam@orange.fr>
2022-11-03 20:53:37 +0100

    modif css

--- ajout.php
+++ ajout.php
@@ -8,8 +8,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="styles/style.css">
+    <link rel="stylesheet" href="styles/navbar.css">
     <title>Ajouter un étudiant</title>
 </head>
 <body>
