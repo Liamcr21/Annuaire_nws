056fcbb802ca60f3a402faa2ec2cacc3313b99ca
Liam Cariou <cariou.liam@orange.fr>
2022-11-03 20:15:07 +0100

    crud supprimer

---
+++ supp.php
@@ -0,0 +1,23 @@
+<?php 
+
+    require_once('./class/bdd.php');
+    $db = new requettes();
+    
+    if(isset($_GET['D_ID']))
+    {
+        global $db;
+        $ID = $_GET['D_ID'];
+
+        if($db->supp_et($ID))
+        {
+            $db->message_ok('<div class="bon">  Etudiant supprimé avec succès</div>');
+            header("location:liste.php");
+        }
+        else
+        {
+            $db->message_ok('<div class="erreur"> Erreur de la suppression (contacter un admin) </div>'); 
+        }
+    }
+
+
+?>
